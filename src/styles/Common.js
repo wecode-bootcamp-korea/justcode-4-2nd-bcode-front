11dda32988125a9d7ef3017707bea0b97c7d8587
lsa <pingu944@gmail.com>
2022-04-19 22:07:35 +0900

    Add : SignUp layout added

--- src/styles/Common.js
+++ src/styles/Common.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const Common = createGlobalStyle`
+*{box-sizing: border-box;}
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
