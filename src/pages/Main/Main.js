0f36984e5be6cbc60fe4f9688dd043df1a0e3b5d
youngseoKim-kr <ysk5754@naver.com>
2022-04-25 17:41:55 +0900

    Add : carouselMain.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,7 @@
 import styled from 'styled-components';
-import CarouselMain from './CarouselMain';
 
 function Main() {
-  return (
-    <>
-      <CarouselMain />
-    </>
-  );
+  return <></>;
 }
 
 export default Main;
